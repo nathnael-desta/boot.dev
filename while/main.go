fcd5faed6baa11363ccd4cb88887643558559b49
Nathnael Desta <destanathnaelataro@gmail.com>
2025-07-14 17:19:09 +0300

    feat: add while

---
+++ while/main.go
@@ -0,0 +1,18 @@
+package main
+
+func getMaxMessagesToSend(costMultiplier float64, maxCostInPennies int) int {
+	actualCostInPennies := 1.0
+	maxMessagesToSend := 1
+	balance := float64(maxCostInPennies) - actualCostInPennies
+	for {
+		actualCostInPennies *= costMultiplier
+		balance -= actualCostInPennies
+		maxMessagesToSend++
+		if balance < 0 {
+			maxMessagesToSend--
+			break
+		}
+	}
+
+	return maxMessagesToSend
+}
