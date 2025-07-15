aa0dd06e9b77ffaba9121bb69276948fbfcfd379
Nathnael Desta <destanathnaelataro@gmail.com>
2025-07-15 10:53:05 +0300

    feat: add count instances

---
+++ mutations/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "errors"
+
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	elem, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
+	}
+	if !elem.scheduledForDeletion {
+		return false, nil
+	}
+	delete(users, name)
+	return true, nil
+}
+
+type user struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
